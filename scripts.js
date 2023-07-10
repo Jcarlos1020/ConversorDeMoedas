1227c40fb6fc57ee1d1de57bf7b4000164908135
Jcarlos1020 <mprojetos14@gmail.com>
2023-07-10 19:33:57 -0300

    SISTEMA DE CONVERSAO JAVASCRIPTS PART 1

--- scripts.js
+++ scripts.js
@@ -4,11 +4,23 @@ function ConvertValues() {
 
     const InputMoedaValor = document.querySelector(".input-moeda").value
 
-    const DolarDia = 5.5
+    const ConverterValor = document.querySelector(".Valor-converter")
 
+    const ConvertidoValor = document.querySelector(".Valor")
+
+    const DolarDia = 5.2
+    
     const ConvertedValue = InputMoedaValor / DolarDia
 
-    console.log( ConvertedValue);
+    ConverterValor.innerHTML =new Intl.NumberFormat("pt-BR", {
+        style:"currency", currency:"BRL"
+    }).format(InputMoedaValor)
+   
+    ConvertidoValor.innerHTML =new Intl.NumberFormat("en-US", {
+        style:"currency", currency:"USD"
+    }).format(ConvertedValue)
+   
+    console.log(ConvertedValue);
 }
 
 
